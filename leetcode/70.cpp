de2d7e7040f7f180eda7b2a0949d7ceb290f6b7b
soumyo001 <www.soumyo@gmail.com>
2025-03-03 03:14:00 +0600

    leetcode sol.

--- leetcode/70.cpp
+++ leetcode/70.cpp
@@ -7,13 +7,31 @@ int ans(int n, vector<int>& dp){
     return dp[n] = ans(n-1,dp) + ans(n-2,dp);
 }
 
+int ansTabulized(int n, vector<int>& dp){
+    dp[0]=dp[1]=1;
+    for(int i=2;i<=n;++i){
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+    return dp[n];
+}
+
+int ansSpaceOptimized(int n, vector<int>& dp){
+    int u = 1, v = 1;
+    int curr;
+    for(int i=2;i<=n;++i){
+        curr = u + v;
+        v = u;
+        u = curr;
+    }
+    return u;
+}
+
 int climbStairs(int n) {
     vector<int> dp(n+1, -1);
     dp[0]=1;
     dp[1]=1;
-    return ans(n,dp);
+    return ansSpaceOptimized(n,dp);
 }
-
 
 int main(void){
     cout<<climbStairs(45);
